d3aa4d108ad75e3d47a8c288f8dcdb72c4d0fda1
Arthur Buzelin <buzelin16@gmail.com>
2023-03-30 15:48:46 -0300

    segundo commit

--- my-app/src/App.jsx
+++ my-app/src/App.jsx
@@ -6,6 +6,14 @@ function App() {
   return (
     <div className="App">
       <div className="main">  {/*Container principal com tudo*/}
+      <p className="top_text">Password Generator</p> {/*Texto inicial*/}
+      <div className="pass_gen">{/*Local onde o texto da password sera gerado*/}
+
+      </div>
+      <div className="main2"> {/*Todo o resto do main*/}
+      <button className='generate'>GENERATE</button> {/* botao de generate*/}
+      </div>
+      
 
       </div>
 
