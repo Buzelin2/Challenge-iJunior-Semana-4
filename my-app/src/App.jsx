c4582d795121b534d319b2806ef3b7e09127189e
Arthur Buzelin <buzelin16@gmail.com>
2023-03-31 16:04:55 -0300

    commit 3

--- my-app/src/App.jsx
+++ my-app/src/App.jsx
@@ -8,7 +8,14 @@ function App() {
       <div className="main">  {/*Container principal com tudo*/}
       <p className="top_text">Password Generator</p> {/*Texto inicial*/}
       <div className="pass_gen">{/*Local onde o texto da password sera gerado*/}
-
+      <p className='password'>P4$5w0rD!</p>
+      </div>
+      <div className='tamanho'>
+        <p className='length'>Character length</p>
+        <p className='length_count'>0</p>
+      </div>
+      <div className='rolagem'>
+        <button className='botao_rolagem'></button>
       </div>
       <div className="main2"> {/*Todo o resto do main*/}
       <button className='generate'>GENERATE</button> {/* botao de generate*/}
